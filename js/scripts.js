4e9e31c4022afe737768ff3b3825fb963cd86340
Rioran <33312275+Rioran@users.noreply.github.com>
2022-06-14 01:19:08 +0300

    + filter js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function filter_vowels() {
     const input_text = document.getElementById("text_input").value;
     const output_div = document.getElementById("text_output");
-    output_div.innerText = input_text.replace();
+    output_div.innerText = input_text.replace(/[^аеёиоуэюяы]/gi, "");
 }
