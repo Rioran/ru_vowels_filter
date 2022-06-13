cf80b97380b1f72c35d6cc9223481e8d36d9c34c
Rioran <33312275+Rioran@users.noreply.github.com>
2022-06-14 02:10:08 +0300

    + js to lowercase

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function filter_text(expression) {
     const input_text = document.getElementById("text_input").value;
     const output_div = document.getElementById("text_output");
-    output_div.innerText = input_text.replace(expression, "");
+    output_div.innerText = input_text.replace(expression, "").toLowerCase();
 }
