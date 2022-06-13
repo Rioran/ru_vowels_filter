10db03aefe73577d80cc071e4c2ad6dc5ef520d4
Rioran <33312275+Rioran@users.noreply.github.com>
2022-06-14 00:51:34 +0300

    + initial files

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function filter_vowels() {
+    var input_text = document.getElementById("text_input");
+    var output_div = document.getElementById("text_output");
+    output_div.innerText = input_text;
+}
