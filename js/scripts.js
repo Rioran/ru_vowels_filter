2b3bc7d8486259cf629023302610e596c815642a
Rioran <33312275+Rioran@users.noreply.github.com>
2022-06-14 03:38:50 +0300

    + copy button

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ function filter_text(expression) {
     const output_div = document.getElementById("text_output");
     output_div.innerText = input_text.replace(expression, "").toLowerCase();
 }
+
+function copy_output() {
+    const text_to_copy = document.getElementById("text_output");
+    text_to_copy.select();
+    text_to_copy.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(text_to_copy.value);
+  }
